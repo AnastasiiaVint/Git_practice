4b42b24219201067525fbbb8567463b41c1c0a3d
AnastasiiaVint <a.vintrovich@gmail.com>
2020-10-14 17:19:42 +0300

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("first commit");
+        System.out.println("second commit");
 	// write your code here
     }
 }
