f3ec9ed89db4a750a8b1e0f26b9c9b90e1352310
AnastasiiaVint <a.vintrovich@gmail.com>
2020-10-14 17:44:21 +0300

    6th commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("second commit");
         System.out.println("third commit");
         System.out.println("fourth commit");
-	// write your code here
+        int i = 10;
+        // write your code here
     }
 }
